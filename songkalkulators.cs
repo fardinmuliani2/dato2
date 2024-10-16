f310cf9101ade544d982ecba6b91fab03154d979
fardinmuliani2 <fardinmuliani38@gmail.com>
2024-10-16 23:39:49 +0400

    Add files via upload

---
+++ songkalkulators.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutA.cs
+{
+    class AnimalFarm : IEnumerable
+    {
+        private List<Animal> _animals = new List<Animal>();
+
+        public AnimalFarm(List<Animal> animalList)
+        {
+            this.animalList = animalList;
+        }
+        public AnimalFarm()
+        {
+
+        }
+        public Animal this[int index]
+        {
+            get { return (Animal)animalList[index]; }
+            set { AnimalList.Insert(index, value); }
+        }
+        public int count
+        {
+            get { return animalList.Count;
+            }
+            IEnumerator IEnumerable.GetEnumerator(){
+            return animalList.getEnumerator();
+            }
+    
+    }
+}
